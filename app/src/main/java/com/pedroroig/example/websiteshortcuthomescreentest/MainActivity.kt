87cb218f81fa11b11ca52c39fdcb729939494969
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2019-01-29 15:10:47 +0100

    Create dropdown list (Spinner) with several web sites

--- app/src/main/java/com/pedroroig/example/websiteshortcuthomescreentest/MainActivity.kt
+++ app/src/main/java/com/pedroroig/example/websiteshortcuthomescreentest/MainActivity.kt
@@ -2,11 +2,38 @@ package com.pedroroig.example.websiteshortcuthomescreentest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spinner = findViewById(R.id.spinnerWebSites)
+        populateSpinnerWithWebsites()
+
+        val buttonCreateShortcut = findViewById<Button>(R.id.buttonCreateShortcut)
+        buttonCreateShortcut.setOnClickListener {
+            createShortcutInHomeMenu()
+        }
+
+    }
+
+    private fun populateSpinnerWithWebsites() {
+
+        val items = arrayOf(
+                "http://www.google.com",
+                "http://www.github.com",
+                "https://developer.android.com")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        spinner.adapter = adapter
+    }
+
+    private fun createShortcutInHomeMenu() {
+        Toast.makeText(this, "Selected: ${spinner.selectedItem}", Toast.LENGTH_SHORT).show()
     }
 }
