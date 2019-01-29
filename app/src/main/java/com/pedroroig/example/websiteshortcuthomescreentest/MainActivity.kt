7ea9b7c97aeda6730e7d2bed8535c487bc40cb3f
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2019-01-29 18:36:27 +0100

    Create the pinned shortcut selected by the user in the spinner

--- app/src/main/java/com/pedroroig/example/websiteshortcuthomescreentest/MainActivity.kt
+++ app/src/main/java/com/pedroroig/example/websiteshortcuthomescreentest/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.pedroroig.example.websiteshortcuthomescreentest
 
-import android.support.v7.app.AppCompatActivity
+import android.app.PendingIntent
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.widget.*
+import android.support.v4.content.pm.ShortcutInfoCompat
+import android.support.v4.content.pm.ShortcutManagerCompat
+import android.support.v4.graphics.drawable.IconCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +44,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createShortcutInHomeMenu() {
-        Toast.makeText(this, "Selected: ${spinner.selectedItem}", Toast.LENGTH_SHORT).show()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            Toast.makeText(this, "Selected: ${spinner.selectedItem}", Toast.LENGTH_SHORT).show()
+
+            if (ShortcutManagerCompat.isRequestPinShortcutSupported(this)) {
+                val title = getTitleFromUrl(spinner.selectedItem as String)
+                val pinShortcutInfo = ShortcutInfoCompat.Builder(this, title)
+                        .setShortLabel(title)
+                        .setLongLabel(title)
+                        .setIcon(IconCompat.createWithResource(this, R.drawable.ic_launcher_foreground))
+                        .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse(spinner.selectedItem as String)))
+                        .build()
+
+
+                // Only notifies if success, if it fails it does nothing
+                val pinnedShortcutCallbackIntent = ShortcutManagerCompat.createShortcutResultIntent(this, pinShortcutInfo)
+
+                val successCallback = PendingIntent.getBroadcast(this, /* request code */ 0,
+                        pinnedShortcutCallbackIntent, /* flags */ 0)
+
+                ShortcutManagerCompat.requestPinShortcut(this, pinShortcutInfo, successCallback.intentSender)
+
+                // TODO Implement callback action if you need to know if creation was successful and do something about it
+
+
+            }
+        } else {
+            Toast.makeText(this, "Your Android version is too old, " +
+                    "it doesn't support pinned shortcuts", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getTitleFromUrl(url: String): String =
+        when(url) {
+            "http://www.google.com" -> "Google"
+            "http://www.github.com" -> "GitHub"
+            "https://developer.android.com" -> "Android Developers"
+            else -> throw IllegalArgumentException("Unexpected URL")
     }
 }
